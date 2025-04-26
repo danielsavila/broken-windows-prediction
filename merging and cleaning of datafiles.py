3dddac2713e1287573a9ba165cafc90db39c8abe
Daniel Avila <danielsavila2020@gmail.com>
2025-04-26 15:24:36 -0500

    completed data cleaning/prep

--- merging and cleaning of datafiles.py
+++ merging and cleaning of datafiles.py
@@ -3,10 +3,12 @@ import numpy as np
 import pandas as pd
 import os
 
-# replace with your local github repo path
-path = "C:/Users/danie/onedrive/documents/github/broken-windows-prediction"
 
-os.chdir(path)
+# replace with your local github repo path
+github_path = "C:/Users/danie/onedrive/documents/github/broken-windows-prediction/raw-data"
+desktop_path = "C:/Users/danie/Desktop"
+
+os.chdir(github_path)
 
 crime2014 = pd.read_csv("Crime 2014.csv")
 crime2015 = pd.read_csv("Crimes 2015.csv")
@@ -30,22 +32,44 @@ for i in range(len(crime_list)):
 
 crime_df.columns = crime_df.columns.str.lower()
 
-crime_df["date"] = crime_df["date"].str[:-11]
+crime_df["date"] = crime_df["date"].str[:-11] # to remove irrelevant time information
 crime_df["date"] = pd.to_datetime(crime_df["date"])
 crime_df["month"] = crime_df["date"].dt.month
 
 # merging community areas (numeric) with community areas (names) to perform groupby
 crime_df = pd.merge(crime_df, communities[["community", "community area"]], how = "left", on = "community area")
 crime_groupby = pd.DataFrame(crime_df.groupby(["year", "month", "community"])["id"].count().reset_index())
-crime_groupby = crime_groupby.rename(columns = {"id" : "count"})
-crime_groupby.head()
+crime_groupby = crime_groupby.rename(columns = {"id" : "monthly_count"})
+crime_groupby = pd.merge(crime_groupby, communities[["community", 'community area']], how = "left", on = "community")
 
+#merging the count of crimes that occurred within each neighborhood for each month each year back into crimes_df
+crime_df = pd.merge(crime_df, crime_groupby, how = "left", on = ["year", "month", "community", "community area"])
+crime_df.head()
 
 
 # graffiti df
 graffiti_df = pd.concat([graffiti1, graffiti2], axis = 0)
 graffiti_df.columns = graffiti_df.columns.str.lower()
-graffiti_df.info()
 graffiti_df["creation date"] = pd.to_datetime(graffiti_df["creation date"], format = "%m/%d/%Y")
 graffiti_df["completion date"] = pd.to_datetime(graffiti_df["completion date"], format = "%m/%d/%Y")
+graffiti_df["year"] = graffiti_df["completion date"].dt.year
+graffiti_df["month"] = graffiti_df["completion date"].dt.month
+graffiti_df = pd.merge(graffiti_df, communities[["community area", "community"]], how = "left", on = "community area")
 
+#potholes df
+potholes.columns = potholes.columns.str.lower()
+potholes = pd.merge(potholes, communities[["community area", "community"]], how = "left", on = "community area")
+potholes["completion date"] = pd.to_datetime(potholes["completion date"])
+potholes["completion date"] = pd.to_datetime(potholes["creation date"])
+potholes["month"] = potholes["completion date"].dt.month
+potholes["year"] = potholes["completion date"].dt.year
+potholes_groupby = pd.DataFrame(potholes.groupby(["year", "month", "community"])["creation date"].count().reset_index())
+potholes_groupby = potholes_groupby.rename(columns = {"creation date": "monthly_count"})
+potholes = pd.merge(potholes, potholes_groupby, how = "left", on = ["year", "month", "community"])
+
+#exporting cleaned datasets to desktop
+#fyi that these end up being massive files
+os.chdir(desktop_path)
+crime_df.to_csv("crime.csv")
+graffiti_df.to_csv("graffiti.csv")
+potholes.to_csv("potholes.csv")
