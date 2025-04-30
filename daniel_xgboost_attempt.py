13865beb9ca77d334d8e0ac1beb625a24e0de436
Daniel Avila <danielsavila2020@gmail.com>
2025-04-30 15:36:58 -0500

    need review of this model

--- daniel_xgboost_attempt.py
+++ daniel_xgboost_attempt.py
@@ -11,12 +11,16 @@ os.chdir(path)
 
 df = pd.read_csv("monthly_count_df.csv").drop("Unnamed: 0", axis = 1)
 
+# NOTE THAT THIS ANALYSIS IS NOT PREDICTING NEXT MONTHS CRIME, IT IS PREDICTING CURRENT MONTH CRIME
+# I.E. PREDICTING JAN CRIME USING JAN POTHOLES + JAN GRAFFITI
+
 pd.set_option("display.max_columns", None)
 pd.set_option("max_colwidth", None)
 
 seed = 1234567
 
 #xgboost does not take in categorical features, need to make the community column categorical
+
 df["community"] = df["community"].astype("category")
 
 x_train, x_test, y_train, y_test = train_test_split(df.loc[:, "year":"monthly_count_potholes"], 
@@ -30,6 +34,7 @@ n_est_list = [n.astype(int) for n in n_est_list]
 rmselist = []
 rsquare_list = []
 
+#also added the "enable_categorical" to accomodate community feature
 for estimator in n_est_list:
     xgb = xgboost.XGBRegressor(max_depth = 4, 
                                n_estimators = estimator, 
