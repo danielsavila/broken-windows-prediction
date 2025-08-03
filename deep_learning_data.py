cc07dde2b77c2ea4fd8951f6742fa45ae2faef62
danielsavila <davila2020@uchicago.edu>
2025-08-03 08:40:23 -0500

    created this file to standardize data across models

---
+++ deep_learning_data.py
@@ -0,0 +1,72 @@
+import os
+import torch
+from torch import functional as f
+from torchinfo import summary
+from torch import nn, optim
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from xgboost_testing import df
+
+#setting up environmental variables for rnn
+#torch.use_deterministic_algorithms(True) #for reproducability
+#torch.backends.cudnn.benchmark = False
+#torch.backends.cudnn.deterministic = True
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE' #for known np package error
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+seed = 123456
+
+scaler = StandardScaler()
+def df_to_tensor(df):
+    df = pd.DataFrame(df)
+    if "community" in df.columns:
+        df = df.drop("community", axis = 1)
+    
+    df = scaler.fit_transform(df)
+    df = torch.Tensor(df).float()
+    return df
+
+
+
+#organizing df by community, then by year and month as prep for rnn
+df = df.groupby("community", group_keys=True, observed = True
+                ).apply(lambda df: df.sort_values(["year_previous", "month_previous"]), include_groups = False
+                ).reset_index().drop("level_1", axis = 1)
+
+#inputting missing values by creating empty df and merging with data
+#this is so rnn has consistent batch sizes later
+mean_crime = df.groupby(["community", "year_previous"])["realized_crime"].mean().reset_index()
+communities = mean_crime["community"].unique()
+years = mean_crime["year_previous"].unique()
+months = np.arange(1,13)
+
+base_df = pd.MultiIndex.from_product([communities, years, months], names = ['community', "year_previous", "month_previous"]).to_frame(index = False)
+base_df = base_df.merge(mean_crime, how = "left", on = ["community", "year_previous"])
+df = base_df.merge(df, how = "left", on = ["community", "year_previous", "month_previous"])
+df["realized_crime_x"] = df["realized_crime_y"].combine_first(df["realized_crime_x"])
+df = df.drop("realized_crime_y", axis = 1).rename({"realized_crime_x":"realized_crime"}, axis = 1)
+df.head()
+
+#shuffle = False to retain temporal construction of df
+x_train, x_test, y_train, y_test = train_test_split(df.loc[:, ["community", "previous_month_graffiti", "previous_month_potholes", "year_previous", "month_previous"]], 
+                                                    df["realized_crime"], 
+                                                    train_size = .7, 
+                                                    random_state = seed, 
+                                                    shuffle = False)
+
+
+#some quick data preprocessing to remove community categorical, reintroduce later
+communities = x_train["community"]
+communities_unique = sorted(communities.unique())
+community_to_id = {name: idx for idx, name in enumerate(communities_unique)}
+community_ids = communities.map(community_to_id).values
+
+
+x_train = df_to_tensor(x_train)
+y_train = df_to_tensor(y_train)
+x_test = df_to_tensor(x_test)
+y_test = df_to_tensor(y_test)
+community_tensor = torch.tensor(community_ids, dtype=torch.long)
